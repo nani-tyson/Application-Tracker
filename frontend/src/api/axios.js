be9c7713f5307695f9d8dc58fec02cbcc79c6ab2
nani-tyson <nanityson10@gmail.com>
2025-08-29 01:02:29 +0530

    Updated deployed Backend Url in frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api", // backend base URL
+  baseURL: "https://application-tracker-vsju.onrender.com/api", // backend base URL
   headers: {
     "Content-Type": "application/json",
   },
