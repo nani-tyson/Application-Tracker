450d67df5d20ad4fb34c3201d6cf0743f3839a2c
nani-tyson <nanityson10@gmail.com>
2025-08-28 14:14:32 +0530

    User setup Completed

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,15 @@ dotenv.config();
 //configs
 import connectDB from "./config/db.js";
 //routes
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   connectDB();
